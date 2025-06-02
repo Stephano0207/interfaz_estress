c3385db63c2e227763cee33e169584d4a97f15d0
Stephano0207 <castanedastephano16@gmail.com>
2025-06-02 07:11:26 -0500

    correccion url

--- app/Http/Controllers/StressPredictionController.php
+++ app/Http/Controllers/StressPredictionController.php
@@ -52,7 +52,8 @@ class StressPredictionController extends Controller
 
         // Opción 1: Llamar a API Flask
         try {
-            $response = Http::post('http://localhost:5000/predict', $input_data);
+            // $response = Http::post('http://localhost:5000/predict', $input_data);
+             $response = Http::post('proyectoprediccionestress-production.up.railway.app/predict', $input_data);
 
 
             if ($response->successful()) {
